37681abd00d58606d70f398fc52ee67d2f759be4
ROOT\swathi.gopal <swathi.gopal@syncfusion.com>
2018-12-31 11:11:58 +0530

    Sample changes committed for showing decimal point in keyboard in android

---
+++ NumKeyBoardDecimalPoint/NumKeyBoardDecimalPoint/NumKeyBoardDecimalPoint/DataFormBehavior.cs
@@ -0,0 +1,59 @@
+﻿using Syncfusion.SfNumericTextBox.XForms;
+using Syncfusion.XForms.DataForm;
+using Syncfusion.XForms.DataForm.Editors;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace NumKeyBoardDecimalPoint
+{
+    public class DataFormBehavior : Behavior<ContentPage>
+    {
+
+        SfDataForm dataForm = null;
+
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            base.OnAttachedTo(bindable);
+
+            dataForm = bindable.FindByName<SfDataForm>("dataForm");
+            dataForm.DataObject = new CompanyInfo();
+
+            if (Device.RuntimePlatform == Device.Android)
+            {
+                dataForm.RegisterEditor("NumericTextBox", new CustomEntryEditor(dataForm));
+                dataForm.RegisterEditor("Salary", "NumericTextBox");
+            }
+            else
+            {
+                dataForm.RegisterEditor("Salary", "Currency");
+            }
+        }
+    }
+
+    public class CompanyInfo
+    {
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public decimal Salary { get; set; }
+    }
+
+    public class CustomEntryEditor : DataFormEditor<CustomEntry>
+    {
+        public CustomEntryEditor(SfDataForm dataForm) : base(dataForm)
+        {
+
+        }
+        protected override CustomEntry OnCreateEditorView(DataFormItem dataFormItem)
+        {
+            return new CustomEntry();
+        }
+
+    }
+
+    public class CustomEntry : SfNumericTextBox
+    {
+
+    }
+}
